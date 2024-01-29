2958c7c99e4c2aa4a7453516bc7d132fb5ef4c18
Adewole Ogunade <108795960+theadewole@users.noreply.github.com>
2024-01-29 16:03:44 +0100

    Add files via upload

--- Datacamp/Data wrangling.R
+++ Datacamp/Data wrangling.R
@@ -103,3 +103,77 @@ gapminder %>%
   filter(year==2007)%>%
   mutate(lifeExpMonths=12*lifeExp)%>%
   arrange(desc(lifeExpMonths))
+
+################################################################################
+# Data Visualization
+################################################################################
+# Load the ggplot2 package as well
+library(gapminder)
+library(dplyr)
+library(ggplot2)
+
+# Create gapminder_1952
+gapminder_1952 <- gapminder %>%
+  filter(year==1952)
+
+# Change to put pop on the x-axis and gdpPercap on the y-axis
+ggplot(gapminder_1952, aes(x = pop, y = gdpPercap)) +
+  geom_point()
+
+# Load the ggplot2 package as well
+library(gapminder)
+library(dplyr)
+library(ggplot2)
+
+# Create gapminder_1952
+gapminder_1952 <- gapminder %>%
+  filter(year==1952)
+
+# Change to put pop on the x-axis and gdpPercap on the y-axis
+ggplot(gapminder_1952, aes(x = pop, y = gdpPercap)) +
+  geom_point()
+
+# Create a scatter plot with pop on the x-axis and lifeExp on the y-axis
+ggplot(gapminder_1952,aes(x=pop,y=lifeExp)) +
+  geom_point()
+########################
+#Log Scale
+#######################
+# Change this plot to put the x-axis on a log scale
+ggplot(gapminder_1952, aes(x = pop, y = lifeExp)) +
+  geom_point() + 
+  scale_x_log10()
+
+# Scatter plot comparing pop and gdpPercap, with both axes on a log scale
+ggplot(gapminder_1952,aes(x=pop,y=gdpPercap))+geom_point()+
+  scale_x_log10()+scale_y_log10()
+
+########################################
+#Additional Aesthetic
+#######################################
+# Scatter plot comparing pop and lifeExp, with color representing continent
+ggplot(gapminder_1952,aes(x=pop,y=lifeExp,color=continent))+
+  geom_point()+
+  scale_x_log10()
+
+# Add the size aesthetic to represent a country's gdpPercap
+ggplot(gapminder_1952, aes(x = pop, y = lifeExp, 
+  color = continent, size=gdpPercap)) +
+    geom_point() +
+scale_x_log10()
+
+##################################
+# Facet
+#################################
+# Scatter plot comparing pop and lifeExp, faceted by continent
+ggplot(gapminder_1952,aes(x=pop,y=lifeExp))+
+  geom_point()+
+  scale_x_log10()+
+  facet_wrap(~continent)
+#Scatter plot comparing gdpPercap and lifeExp, with color representing continent
+ ##and size representing population, faceted by year
+ggplot(gapminder,aes(x=gdpPercap,y=lifeExp,
+  color=continent,color=pop))+
+  geom_point()+
+  scale_x_log10()+
+  facet_wrap(~year)
